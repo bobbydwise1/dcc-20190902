15c80f44ab939bbb8f1f0ee644869dfc67b5ea8c
Robert Lee <bobbydwise1@hotmail.com>
2019-09-02 20:27:26 -0700

    Add set function.

--- scripts.js
+++ scripts.js
@@ -11,10 +11,35 @@ Implement an LRU (Least Recently Used) cache. It should be able to be initialize
 Each operation should run in O(1) time.
 */
 
+class leastRecentlyUsedCache{
+  constructor(n=1) {
+    this.n = n
+    this.cache = []
+  }
 
+  set(key,value) {
+    if (this.cache.length < this.n) {
+      this.cache.push({'key': key, 'value': value})
+    } else {
+      this.cache.shift()
+      this.cache.push({'key': key, 'value': value})
+    }
+    return this.cache
+  }
+}
+
+let test0 = new leastRecentlyUsedCache(5)
+console.log(test0)
+test0.set('name','Joe')
+test0.set('name','Jane')
+test0.set('name','Jack')
+test0.set('name','Juile')
+test0.set('name','Julian')
+test0.set('name','Janice')
+console.log(test0)
 
 $(document).ready(function() {
-  $('#output-section-0').text(JSON.stringify(deck))
+  $('#output-section-0').text(1)
   $('#form1').submit(function(){
     event.preventDefault()
     $('#output-section-1').text(1)
